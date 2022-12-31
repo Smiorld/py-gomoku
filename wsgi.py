ec025d9545aa8328fc771e0fb4e642fcfd329cac
Julius <q1541599745@gmail.com>
2022-12-31 19:13:01 +0000

    update port

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app import create_app, socketio, db, login_manager
 
 app = create_app(make_db=True,debug=True)
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0',port=5000)
+    socketio.run(app,host='0.0.0.0',port=80)
 
 
 
